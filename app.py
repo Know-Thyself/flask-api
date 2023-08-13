c90917b703daf8919c5b66929130e5bdc0795b64
Biruk Kebede <biruk.key@gmail.com>
2023-08-13 05:34:49 +0100

    creating store and adding items api endpoints

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -14,7 +14,28 @@ stores = [
 ]
 
 
-# Stores route/endpoint
+# Get stores route/endpoint
 @app.get("/stores")
 def get_stores():
     return {"stores": stores}
+
+
+# Create a store
+@app.post("/store")
+def create_store():
+    store = request.get_json()
+    new_store = {"name": store["name"], "items": []}
+    stores.append(new_store)
+    return new_store, 201
+
+
+# Adding item
+@app.post("/store/<string:name>/item")
+def add_item(name):
+    item = request.get_json()
+    new_item = {"name": item["name"], "price": item["price"]}
+    for store in stores:
+        if store["name"] == name:
+            store["items"].append(new_item)
+            return new_item, 201
+    return {"message": "Store not found!"}, 404
