bc510690bb48ac8e3644ff7bf86470252fbc2719
Biruk Kebede <biruk.key@gmail.com>
2023-08-20 00:09:14 +0100

    Connecting postgreSQL database

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from resources.tag import blp as TagBlueprint
 from resources.user import blp as UserBlueprint
 from flask_jwt_extended import JWTManager
 from blocklist import BLOCKLIST
+from dotenv import load_dotenv
 import os
 
 # from os import environ
@@ -15,6 +16,7 @@ from flask_migrate import Migrate
 
 def create_app(db_url=None):
     app = Flask(__name__)
+    load_dotenv()
 
     app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["API_TITLE"] = "Stores REST API"
@@ -25,10 +27,10 @@ def create_app(db_url=None):
     app.config[
         "OPENAPI_SWAGGER_UI_URL"
     ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    # app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv(
-    #     "DATABASE_URL", "sqlite:///data.db"
-    # )
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///data.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv(
+        "DATABASE_URL", "sqlite:///data.db"
+    )
+    # app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///data.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     migrate = Migrate(app, db)
